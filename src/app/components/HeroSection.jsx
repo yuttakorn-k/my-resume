71a712ae39c90987080dca7cde5e4eeda499eb30
yuttakornk <yuttakorn.kaey@e-tech.ac.th>
2024-07-29 13:37:20 +0700

    แก้ไข HeroSection.jsx: ปรับปรุงการแสดงผล UI และแก้ไขบั๊กการแสดงผล

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -46,6 +46,8 @@ const HeroSection = () => {
                     <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                         <Image src="/images/resume_image.jpg" width={300} height={300}
                             className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 ' />
+                        <Image src="/images/resume_image.jpg" width={300} height={300}
+                            className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 ' />
                     </div>
                 </motion.div>
             </div>
